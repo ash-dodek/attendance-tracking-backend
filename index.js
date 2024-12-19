309eb1c99c60cd21b60d0edac5e65d3ea0741d14
Harsh <harshtrip2007@gmail.com>
2024-12-20 00:20:30 +0530

    fix related to deployment

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
+const PORT = process.env.PORT || 3000
+
 const allowedOrigins = process.env.ORIGIN.split(',')
 try {
     
